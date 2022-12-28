1b5db249c23a0e029323718f3943aa6181307fe0
kimbodinii <115832645+kimbodinii@users.noreply.github.com>
2022-12-27 18:14:44 -0800

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,11 +2,27 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, spent, dispatch } = useContext(AppContext);
+
+    const handleChange = (event) => {
+        const {value} = event.target
+
+        if(value > 20000) {
+            alert('Value cannot be greater than 20000')
+            return
+        }
+        if (value < spent) {
+            alert(`Value cannot be less than what has already been spent: ${spent}`)
+            return
+        }
+
+        dispatch({type: 'SET_BUDGET', payload: event.target.value})
+    }
 
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £</span>
+            <input type='number' step='10' min='0' max='20000' value={budget} onChange={handleChange} />
         </div>
     );
 };
